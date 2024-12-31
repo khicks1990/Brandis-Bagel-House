6d4e55cbc124d446cace494a81e7124dc9d48b39
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 07:03:47 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+       public static void main(String[] args)
+       {
+          new OrderCalculatorGUI();
+       }
+}
